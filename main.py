2d20021c03a25cbd5e3d0bec59d5daf3bc84ee01
仝朋成 <tongpc@idx365.com>
2020-03-10 14:46:33 +0800

    update: 调整模板图片的位置; 优化出击敌军的逻辑;

--- main.py
+++ main.py
@@ -22,22 +22,22 @@ def run():
             exit(1)
 
         # 处理自律战斗的提示
-        res = auto_adb.wait('temp_images/auto-fight-confirm-1.png', max_wait_time=3).click()
+        res = auto_adb.wait('temp_images/fight/auto-fight-confirm-1.png', max_wait_time=3).click()
         if res:
             print('确认自律战斗')
-            auto_adb.wait('temp_images/auto-fight-confirm-2.png').click()
+            auto_adb.wait('temp_images/fight/auto-fight-confirm-2.png').click()
 
         # 找到敌人后开始出击
-        auto_adb.wait('temp_images/fight.png').click()
+        auto_adb.wait('temp_images/fight/fight.png').click()
         check_port_full()
 
         print('战斗开始 ...')
-        fight_finish_loc = auto_adb.wait('temp_images/fight-finish.png')
+        fight_finish_loc = auto_adb.wait('temp_images/fight/fight-finish.png')
         print(' 战斗结束 !')
         fight_finish_loc.click()
-        auto_adb.wait('temp_images/fight-finish-1.png').click()
-        auto_adb.wait('temp_images/fight-finish-1.5.png', max_wait_time=3).click()
-        auto_adb.wait('temp_images/fight-finish-2.png').click(3)
+        auto_adb.wait('temp_images/fight/fight-finish-1.png').click()
+        auto_adb.wait('temp_images/fight/fight-finish-1.5.png', max_wait_time=3).click()
+        auto_adb.wait('temp_images/fight/fight-finish-2.png').click(3)
 
 
 # 招惹敌军
@@ -46,31 +46,35 @@ def provoke_enemy():
     image_dir = 'temp_images/enemy'
     image_name_list = os.listdir(image_dir)
 
+    enemy_loc = None
     for image_name in image_name_list:
         image_rel_path = image_dir + '/' + image_name
-        loc = auto_adb.get_location(image_rel_path)
-        if loc is not None:
-            loc.click()
-            # 处理途中获得道具的提示
-            auto_adb.wait('temp_images/get-tool.png', max_wait_time=3).click()
-            loc.click()
+        enemy_loc = auto_adb.get_location(image_rel_path)
+        if enemy_loc is not None:
+            break
+    if enemy_loc is None:
+        print('找不到敌机')
+        return False
+    while True:
+        enemy_loc.click()
+        is_valuable = auto_adb.wait('temp_images/fight/fight.png', max_wait_time=2).is_valuable()
+        if is_valuable:
             return True
-    # todo 尝试滑动界面
-    print('找不到敌机')
-    return False
+        # 处理途中获得道具的提示
+        auto_adb.wait('temp_images/round/get-tool.png', max_wait_time=1).click()
 
 
 # 选择关卡
 def pick_round():
     auto_adb = AutoAdb()
-    res = auto_adb.wait('temp_images/in-round.png', max_wait_time=5).is_valuable()
+    res = auto_adb.wait('temp_images/round/in-round.png', max_wait_time=5).is_valuable()
     if res:
         return
 
     check_port_full()
     # 确定进入
     auto_adb.wait('temp_images/round/4-3.png').click()
-    auto_adb.wait('temp_images/fight-confirm.png').click()
+    auto_adb.wait('temp_images/round/into-confirm.png').click()
 
 
 # 判断船坞是否满员
