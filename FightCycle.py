7d50b9d4838d4319d3aababac3889e6c5c112b5c
仝朋成 <tongpc@idx365.com>
2020-03-24 08:46:15 +0800

    update: 优化循环逻辑；

--- FightCycle.py
+++ FightCycle.py
@@ -3,20 +3,22 @@ from common.AutoAdb import AutoAdb
 
 
 def run(cycle=True):
+    num = 0
+
     auto_adb = AutoAdb(test_device=True)
     res = auto_adb.check('temp_images/stage/in-stage.png')
     if res:  # 如果已经在关卡中，则继续战斗
+        num += 1
         StageFight.fight_in_stage()
 
     # 循环战斗
     while True:
+        if not cycle and num >= 1:
+            break
         # 选择关卡
         StageFight.pick_stage()
         # 开始战斗
         StageFight.fight_in_stage()
-        # 判断是否循环
-        if not cycle:
-            break
 
 
 if __name__ == '__main__':
