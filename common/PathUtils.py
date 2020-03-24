07e2fd77beb3b2b88114e32fed98bdd913b9e907
仝朋成 <tongpc@idx365.com>
2020-03-24 19:49:02 +0800

    fix #2: 修复项目第一次运行时文件存储报错的问题；

--- common/PathUtils.py
+++ common/PathUtils.py
@@ -9,7 +9,15 @@ def get_work_dir():
 
 # 获取项目的缓存路径
 def get_cache_dir():
-    return get_work_dir() + '/cache'
+    cache_dir = get_work_dir() + '/cache'
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+        return cache_dir
+    if not os.path.isdir(cache_dir):
+        os.remove(cache_dir)
+        os.makedirs(cache_dir)
+        return cache_dir
+    return cache_dir
 
 
 # 根据相对路径生成绝对路径
