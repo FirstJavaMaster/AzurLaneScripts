cef406d081ae5e352f4387e47388e9614af5fc9c
仝朋成 <tongpc@idx365.com>
2020-04-13 16:28:34 +0800

    fix：计时器初始时间设置失效的问题；

--- common/Timer.py
+++ common/Timer.py
@@ -2,7 +2,10 @@ from datetime import datetime
 
 
 class Timer:
-    start_time = datetime.now()
+    start_time = None
+
+    def __init__(self):
+        self.start_time = datetime.now()
 
     def get_duration(self):
         return (datetime.now() - self.start_time).seconds
