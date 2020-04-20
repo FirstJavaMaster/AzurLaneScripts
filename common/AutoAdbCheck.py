5b91c952ad35d3bd35ae2ecf682f7ea2767c3018
仝朋成 <tongpc@idx365.com>
2020-04-20 15:19:19 +0800

    update：adb设备检查离线状态；

--- common/AutoAdbCheck.py
+++ common/AutoAdbCheck.py
@@ -27,29 +27,37 @@ def check_link(auto_adb):
         print('未检测到设备，请检查ADB地址配置，或参考 https://github.com/FirstJavaMaster/AzurLaneScripts/blob/master/README.md')
         exit(1)
 
-    exist = False
-    print()
-    print('设备列表：')
+    target_device = None
+    print('\n设备列表：')
     for device in device_list:
         print(device)
-        if adb_host_port == device:
-            exist = True
-    if not exist:
-        print('设备列表中没找到目标连接（%s），请检查配置是否正确')
-        exit(1)
+        if adb_host_port in device:
+            target_device = device
 
+    print()
+    # 设备没找到
+    if target_device is None:
+        print('设备列表中没找到目标连接（%s），请检查配置是否正确' % adb_host_port)
+        exit(1)
+    # 如果找到了设备，但是离线状态
+    if 'offline' in target_device:
+        print('检测到设备离线！！！')
+        print('由于不同模拟器ADB实现细节不同，请尝试以下办法（没有先后顺序）：')
+        print('· 重启模拟器')
+        print('· 如果模拟器已经自带了ADB，则可以尝试将脚本中adb文件夹的文件拷贝至模拟器自带ADB目录。注意备份原文件')
+        print('· 在adb目录下打开cmd，执行 adb kill-server 后再执行 adb start-server，然后执行 adb devices 查看是否解决')
+        print('· 重启电脑')
+        print('· 换个模拟器')
+        exit(1)
+    # 设备正常
     print('设备已连接', end='\n\n')
 
 
 # 获取设备列表
 def get_devices(auto_adb):
-    new_lines = []
     lines = auto_adb.run('devices').splitlines()
-
     del (lines[0])  # 删除第一个标题行
-    for line in lines:
-        new_lines.append(line.split()[0])
-    return new_lines
+    return lines
 
 
 def check_size(auto_adb):
