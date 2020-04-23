3a82b5264300bd56eefaf1c036918582985ce263
仝朋成 <tongpc@idx365.com>
2020-04-23 16:09:55 +0800

    update：优化战斗结束时的判断；

--- bin/StageFight.py
+++ bin/StageFight.py
@@ -1,5 +1,3 @@
-import time
-
 from common import PortUtils, PageUtils
 from common.AutoAdb import AutoAdb
 from common.Location import Location
@@ -67,12 +65,6 @@ def fight_all_enemy():
     while True:
         team_leader.provoke_enemy()
         fight()
-        # 如果在演习界面
-        if PageUtils.in_operation_page():
-            print('误入演习界面，退出')
-            PageUtils.back()
-            time.sleep(1)
-
         # 如果在单元界面，说明关卡已经结束
         if PageUtils.in_stage_page():
             print('关卡战斗结束')
